b67da539fc206e704aeb2e4af884c14a8fa759f1
John-Dig <rohnfiggins@gmail.com>
2023-01-27 09:22:07 -0800

    .2 passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 //Business Logic
 function Pizza() {
   this.size = 0;
-  this.toppings = {};
+  this.toppings = [];
   this.cost = 0;
 }
 
 Pizza.prototype.costCalculator =function() {
-  this.cost = length(this.toppings);
+  this.cost = this.toppings.length * 1.5 + this.size * 2;
+  return "$" + this.cost.toFixed(2);
 }
+
+//temp console
+let pizza1 = new Pizza;
+pizza1.toppings.push("chicken");
+pizza1.toppings.push("peppers");
+pizza1.toppings.push("sauce");
+pizza1.size = 3;
+
