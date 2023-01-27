02e55e09b08c86bf76685c928c7c2490601bf1b1
John-Dig <rohnfiggins@gmail.com>
2023-01-27 09:51:52 -0800

    .21 passed, checkout works

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ function Pizza() {
   this.size = 0;
   this.toppings = [];
   this.cost = 0;
+  this.customer = "";
 }
-
-Pizza.prototype.costCalculator =function() {
+Pizza.prototype.checkout =function() {
   this.cost = this.toppings.length * 1.5 + this.size * 2;
-  return "$" + this.cost.toFixed(2);
+  return this.customer + ":  $" + this.cost.toFixed(2);
 }
 
 //temp console
@@ -16,4 +16,7 @@ pizza1.toppings.push("chicken");
 pizza1.toppings.push("peppers");
 pizza1.toppings.push("sauce");
 pizza1.size = 3;
+pizza1.customer = "Richard"
+
+
 
